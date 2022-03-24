2d005e12adef7411e2577e80414a4a2eb25afb41
sujanrijal240 <99158462+sujanrijal240@users.noreply.github.com>
2022-03-24 15:59:24 +0545

    sujan

--- script.js
+++ script.js
@@ -14,34 +14,40 @@ $(document).ready(function(){
             $('.scroll-up-btn').removeClass("show");
         }
     });
+
     // slide-up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
+
     $('.navbar .menu li a').click(function(){
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
+
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Tiktoker", "Nakali", "Blogger", "Maskwali", "Ramri kt"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
+
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Tiktoker", "Nakali", "Blogger", "Maskwali", "Ramri kt"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
+
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
